31accb349bdce16e2901f7e687dc7ae139427030
janani <jananiob@gmail.com>
2022-02-17 22:19:08 +0530

    Images modified

--- scripts/videos_page.js
+++ scripts/videos_page.js
@@ -1,102 +1,64 @@
 function setNewImage1()
 {
-    
-    
-    document.getElementById("img1").src="https://i.ytimg.com/vi/jRFKZGWrmrM/maxresdefault.jpg";
-   
+    document.getElementById("img1").src="https://i.ytimg.com/vi/jRFKZGWrmrM/maxresdefault.jpg"; oooooo
 }
 function setOldImage1()
 {
-    
-    document.getElementById("img1").src="https://cdn.educba.com/academy/wp-content/uploads/2016/03/Python-Programming-for-the-Absolute-Beginner.jpg";
-    
+    document.getElementById("img1").src="https://i.ytimg.com/vi/r9lR5hLgEnI/hqdefault.jpg?sqp=-oaymwEcCNACELwBSFXyq4qpAw4IARUAAIhCGAFwAcABBg==&rs=AOn4CLBB3tNZ-tv_c1CQh1u-YoT_ywzz3w";
 }
 function setNewImage2()
-{
-    
-    
-    document.getElementById("img2").src="https://code.visualstudio.com/assets/docs/languages/javascript/overview.png";
-   
+{ 
+    document.getElementById("img2").src="https://code.visualstudio.com/assets/docs/languages/javascript/overview.png"; oooooo
 }
 function setOldImage2()
 {
-    
-    document.getElementById("img2").src="https://www.game-designers.net/wp-content/uploads/2019/12/JavaScript-Tutorial-for-Beginners-Learn-JavaScript-in-1-Hour-2019.jpg";
-    
+    document.getElementById("img2").src="https://i.ytimg.com/vi/ATElufr0OiE/hqdefault.jpg?sqp=-oaymwEcCNACELwBSFXyq4qpAw4IARUAAIhCGAFwAcABBg==&rs=AOn4CLCJuaATtZxg3dXNAyUiTBM-dVbiOA";
 }
 function setNewImage3()
 {
-
-    document.getElementById("img3").src="https://images.idgesg.net/images/article/2019/03/javaworld_jvm_jdk_jre_explainer_java_development_kit_write_3x2_2400x1600_by_idg_oracle_vasabii_gettyimages-100790551-large.jpg?auto=webp&quality=85,70";
-   
+    document.getElementById("img3").src="https://images.idgesg.net/images/article/2019/03/javaworld_jvm_jdk_jre_explainer_java_development_kit_write_3x2_2400x1600_by_idg_oracle_vasabii_gettyimages-100790551-large.jpg?auto=webp&quality=85,70"; ooooo
 }
 function setOldImage3()
 {
-    
-    document.getElementById("img3").src="https://i.ytimg.com/vi/eIrMbAQSU34/maxresdefault.jpg";
-    
+    document.getElementById("img3").src="https://i.ytimg.com/vi/G5ur_C-L7ZU/hqdefault.jpg?sqp=-oaymwEcCNACELwBSFXyq4qpAw4IARUAAIhCGAFwAcABBg==&rs=AOn4CLChu2FKD3CYttkRG1m739X7tdi8MQ";
 }
 function setNewImage4()
 {
-    
-     document.getElementById("img4").src="https://www.habilelabs.io/wp-content/uploads/2021/02/This-is-Why-You-Should-Choose-React-js-1.jpg";
-   
+     document.getElementById("img4").src="https://www.habilelabs.io/wp-content/uploads/2021/02/This-is-Why-You-Should-Choose-React-js-1.jpg"; ooooo
 }
 function setOldImage4()
-{
-    
-    document.getElementById("img4").src="https://process.fs.teachablecdn.com/ADNupMnWyR7kCWRvm76Laz/resize=width:705/https://www.filepicker.io/api/file/fGWjtyQtG4JE7UXgaPAN";
-    
+{ 
+    document.getElementById("img4").src="https://i.ytimg.com/vi/jmEJsv9WT8s/hqdefault.jpg?sqp=-oaymwEcCNACELwBSFXyq4qpAw4IARUAAIhCGAFwAcABBg==&rs=AOn4CLDJQwLQeymT53HaC_fZ5LBOJ0G6fQ"; 
 }
 function setNewImage5()
 {
-    
-    
-    document.getElementById("img5").src="https://merehead.com/blog/wp-content/uploads/11-5.png";
-   
+    document.getElementById("img5").src="https://merehead.com/blog/wp-content/uploads/11-5.png"; ooooo
 }
 function setOldImage5()
 {
-    
-    document.getElementById("img5").src="https://ourcodeworld.com/public-media/articles/articleocw-5d653602443b7.webp";
-    
+    document.getElementById("img5").src="https://i.ytimg.com/vi/fRD_3vJagxk/hqdefault.jpg?sqp=-oaymwEcCNACELwBSFXyq4qpAw4IARUAAIhCGAFwAcABBg==&rs=AOn4CLAEYn21IC3IEXU-3mSFIsWUzouLtQ";
 }
 function setNewImage6()
 {
-   
-    
-    document.getElementById("img6").src="https://static.javatpoint.com/tutorial/git/images/git-benefits.png";
-   
+ document.getElementById("img6").src="https://static.javatpoint.com/tutorial/git/images/git-benefits.png"; oooo 
 }
 function setOldImage6()
-{
-    
-    document.getElementById("img6").src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQKX2rc3-Um-Vw0kH9gEDe35s8C61TwXBTvMw&usqp=CAU";
-    
+{ 
+    document.getElementById("img6").src="https://i.ytimg.com/vi/gjOLk0L830c/hqdefault.jpg?sqp=-oaymwEcCNACELwBSFXyq4qpAw4IARUAAIhCGAFwAcABBg==&rs=AOn4CLDvZZweHjjjcOKMR_jA3E8NryqvQg";
 }
 function setNewImage7()
 {
-    
-    
-    document.getElementById("img7").src="https://dotnettrickscloud.blob.core.windows.net/img/angular/angular-component-page-view.png";
-   
+    document.getElementById("img7").src="https://dotnettrickscloud.blob.core.windows.net/img/angular/angular-component-page-view.png"; oooo
 }
 function setOldImage7()
 {
-    
-    document.getElementById("img7").src="https://i.morioh.com/2020/02/03/32bd329b87ed.jpg";
-    
+    document.getElementById("img7").src="https://i.ytimg.com/vi/00fWlZnZAo0/hqdefault.jpg?sqp=-oaymwEcCNACELwBSFXyq4qpAw4IARUAAIhCGAFwAcABBg==&rs=AOn4CLBwEtGQktNejGrNI2dqq9Ngt0UibA";
 }
 function setNewImage8()
-{
-    
-    
-    document.getElementById("img8").src="https://cdn-media-1.freecodecamp.org/images/1*4psHn7qR2v1O5kBhJQ_T2w.png";
-   
+{ 
+    document.getElementById("img8").src="https://cdn-media-1.freecodecamp.org/images/1*4psHn7qR2v1O5kBhJQ_T2w.png"; oooooo
 }
 function setOldImage8()
 {
-    
-    document.getElementById("img8").src="https://www.hostingclues.com/wp-content/uploads/Learning-HTML-for-Beginners-Banners.png";
-    
+    document.getElementById("img8").src="https://i.ytimg.com/vi/A_z5g0_hJN8/hqdefault.jpg?sqp=-oaymwEcCNACELwBSFXyq4qpAw4IARUAAIhCGAFwAcABBg==&rs=AOn4CLCief-B1bUkymcxXaMHrwAa4psRmA"; 
 }
